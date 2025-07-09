1cdee9d2c5bf2e852fb7a1a461f37615f2c2d2cb
USUARIO <antholozadarequelme@gmail.com>
2025-07-08 22:27:34 -0500

    add implementation of binary search in BuBinaria class

--- src/BuBinaria.java
+++ src/BuBinaria.java
@@ -1,5 +1,26 @@
 ﻿public class BuBinaria {
-    public static void main(String[] args) {
+    public static int busquedaBinaria(int[] arreglo, int elementoBuscar){
+        int inicio = 0;
+        int fin = arreglo.length - 1;
 
+        while(inicio <= fin){
+            int medio = inicio + (fin - inicio)/2;
+            if(arreglo[medio] == elementoBuscar){
+                return medio;
+            }
+            if(arreglo[medio] < elementoBuscar){
+                inicio = medio + 1; // ignorar la mitad izquierda
+            }
+            else{
+                fin = medio - 1; // ignora la mitad derecha;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arreglo = {2, 5, 8, 12, 16, 23, 38, 56, 72};
+        int elementoBuscar = 23;
+        int indice = busquedaBinaria(arreglo, elementoBuscar);
     }
 }
