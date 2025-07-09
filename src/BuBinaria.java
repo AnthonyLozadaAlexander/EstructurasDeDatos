f00a74554b1f5d88755cfd52426f1ad5c4414933
USUARIO <antholozadarequelme@gmail.com>
2025-07-08 22:34:41 -0500

    add user input and feedback for binary search in BuBinaria class

--- src/BuBinaria.java
+++ src/BuBinaria.java
@@ -1,4 +1,6 @@
-﻿public class BuBinaria {
+﻿import javax.swing.*;
+
+public class BuBinaria {
     public static int busquedaBinaria(int[] arreglo, int elementoBuscar){
         int inicio = 0;
         int fin = arreglo.length - 1;
@@ -20,7 +22,18 @@
 
     public static void main(String[] args) {
         int[] arreglo = {2, 5, 8, 12, 16, 23, 38, 56, 72};
-        int elementoBuscar = 23;
+
+        int elementoBuscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero a buscar: "));
         int indice = busquedaBinaria(arreglo, elementoBuscar);
+
+        if(indice == -1){
+            JOptionPane.showMessageDialog(null, "Numero: " + elementoBuscar + " no encontrado",
+                    "Resultado de la busqueda binaria", JOptionPane.ERROR_MESSAGE);
+
+        }else{
+            JOptionPane.showMessageDialog(null, "Numero: " + elementoBuscar + " encontrado en el indice["+indice+"]",
+                    "Resultado de la busqueda binaria", JOptionPane.INFORMATION_MESSAGE);
+        }
+
     }
 }
