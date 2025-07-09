4b8da729e1e9eb6abf62fe7a7899dc5943b2304f
USUARIO <antholozadarequelme@gmail.com>
2025-07-08 21:31:16 -0500

    refactor variable name in BuSecuencial class for clarity

--- src/BuSecuencial.java
+++ src/BuSecuencial.java
@@ -3,14 +3,14 @@ import javax.swing.*;
 public class BuSecuencial {
     public static void main(String[] args) {
         int arreglo[] = {4, 1, 5, 2, 3};
-        int dato;
+        int num;
         boolean encontrado = false;
         int posicion = -1;
 
-        dato = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el dato a buscar: "));
+        num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el dato a buscar: "));
 
         for (int i = 0; i < arreglo.length; i++) {
-            if(arreglo[i] == dato){
+            if(arreglo[i] == num){
                 encontrado = true;
                 posicion = i;
                 break;
@@ -18,9 +18,9 @@ public class BuSecuencial {
         }
 
         if(encontrado){
-            JOptionPane.showMessageDialog(null, "El num: " + dato + " se encuentra en la posicion["+posicion+"]");
+            JOptionPane.showMessageDialog(null, "El num: " + num + " se encuentra en la posicion["+posicion+"]");
         } else {
-            JOptionPane.showMessageDialog(null, "El num: " + dato + " no se encuentra en el arreglo");
+            JOptionPane.showMessageDialog(null, "El num: " + num + " no se encuentra en el arreglo");
         }
     }
 }
